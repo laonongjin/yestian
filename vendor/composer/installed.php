d8d40681fef1a1008a0989d2e9676a5de33f723d
69719701@qq.com <yestian@laonongjin.com>
2023-03-26 00:02:36 +0800

    d

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
-        'name' => 'yestian/yestian',
+        'reference' => 'f3a97a5932a901356660b3fe7fab3f97fe734a1b',
+        'name' => 'yestian/pc',
         'dev' => true,
     ),
     'versions' => array(
-        'yestian/yestian' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+        'yestian/pc' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => 'f3a97a5932a901356660b3fe7fab3f97fe734a1b',
             'dev_requirement' => false,
         ),
     ),
